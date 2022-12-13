808eb1cdc3cfe5a525ed4b9c0ff29f1b32985b0c
jongwoo <jongwoo@codemind.co.kr>
2022-12-13 14:48:20 +0900

    clang plugin FunctionMatcher 추가

--- engines/clang-plugin/CrichtonConsumer.cpp
+++ engines/clang-plugin/CrichtonConsumer.cpp
@@ -2,7 +2,33 @@
 
 using namespace Crichton;
 
+class CrichtonMatcher : public MatchFinder::MatchCallback {
+  protected:
+    CrichtonASTConsumer *consumer;
+  public:
+    CrichtonMatcher(CrichtonASTConsumer *cs) : consumer(cs) {
+    }
+
+    SourceManager &getSourceManager() {
+      return consumer->getSourceManager();
+    }
+};
+
+class FunctionMatcher : public CrichtonMatcher {
+  public:
+    FunctionMatcher(CrichtonASTConsumer *cs) : CrichtonMatcher(cs) {}
+    void run(const MatchFinder::MatchResult &Results) override {
+      auto fd = Results.Nodes.getNodeAs<FunctionDecl>("function");
+      outs() << fd->getQualifiedNameAsString() << "\n";
+    }
+};
+
 void CrichtonASTConsumer::HandleTranslationUnit(ASTContext &ctx) {
-  outs() << "11111111\n";
+  auto codegenFunction = make_shared<FunctionMatcher>(this);
+  addMatcher(codegenFunction);
+  finder.addMatcher(
+    functionDecl().bind("function"),
+    codegenFunction.get());
+
   finder.matchAST(ctx);
 }
