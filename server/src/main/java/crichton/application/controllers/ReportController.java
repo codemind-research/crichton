f1722e08d6f94aca63476b6c4a9191d14d162708
hanjin <gkswls1117@gmail.com>
2023-12-05 16:33:48 +0900

    report api 설명 추가

--- server/src/main/java/crichton/application/controllers/ReportController.java
+++ server/src/main/java/crichton/application/controllers/ReportController.java
@@ -5,10 +5,12 @@ import crichton.domain.dtos.ReportDTO;
 import crichton.domain.services.ReportService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Report Controller", description = "This API is a controller responsible for processing Report Data to be used by the client.")
 @CrossOrigin
 @RestController("ReportController")
 @RequestMapping("/api/v1/crichton/report")
@@ -18,7 +20,8 @@ public class ReportController {
     private final ReportService reportService;
 
     @GetMapping("/data")
-    @Operation(summary = "Client html 레포트 데이터 가공", description = "Plugins 에서 떨군 csv 레포트를 클라이언트에서 사용할 수 있게 데이터를 가공한다.")
+    @Operation(summary = "Process Client HTML Report Data", description = "Processes CSV reports sent by plugins to make the data available for the client." +
+            " This API is responsible for transforming the provided CSV reports into a format usable by the client for generating HTML reports.")
     public ResponseEntity<ReportDTO.DataResponse> getData() throws CustomException {
         ReportDTO.DataResponse data = reportService.getData();
         return ResponseEntity.ok().body(data);
