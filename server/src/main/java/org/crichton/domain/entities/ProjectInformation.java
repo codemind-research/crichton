f72996e90288265729b88f8632bf7e2c29f67748
juniair.kim <kkj@codemind.co.kr>
2024-10-11 17:09:23 +0900

    [feature] UUID와 String을 변환하는 클래스 추가
    - Entity에 해당 컨버터 사용하도록 어노테이션 추가

--- server/src/main/java/org/crichton/domain/entities/ProjectInformation.java
+++ server/src/main/java/org/crichton/domain/entities/ProjectInformation.java
@@ -1,16 +1,27 @@
 package org.crichton.domain.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.crichton.domain.utils.converters.UUIDToStringConverter;
+
+import java.util.UUID;
 
 @Getter
 @NoArgsConstructor
 @Entity
 public class ProjectInformation {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Convert(converter = UUIDToStringConverter.class)
+    private UUID uuid;
 
+    @Builder
+    public ProjectInformation(UUID uuid) {
+        this.uuid = (uuid != null) ? uuid : UUID.randomUUID(); // UUID가 null이면 자동 생성
+    }
 }
