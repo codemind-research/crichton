c8a98e11523777b44eafbb1ca7f71e9e1c3de2df
juniair.kim <kkj@codemind.co.kr>
2024-10-14 18:25:23 +0900

    [feature] 분석 대상 데이터 저장 항목 위치 properties에서 설정하도록 추가

--- server/src/main/java/org/crichton/ServerApplication.java
+++ server/src/main/java/org/crichton/ServerApplication.java
@@ -1,37 +1,33 @@
 package org.crichton;
 
 import org.crichton.configuration.Config;
+import org.crichton.configuration.CrichtonDataStorageProperties;
 import org.crichton.util.ManifestUtil;
-import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@EnableConfigurationProperties(CrichtonDataStorageProperties.class)
 public class ServerApplication {
     private static Config config;
     private static ConfigurableApplicationContext APP_CTX;
 
     public static void main(String[] args)  {
-        if(args.length == 0){
-            System.err.println("Usage : crichton server [port]");
-            return;
-        }
-        config = new Config();
-        config.setPort(Integer.parseInt(args[0]));
+
+
         ServerApplication.Version.set();
         System.out.println("Crichton Server Version: " + Version.getBuildInfo());
         application(args, true);
     }
 
     private static void application(String[] args, boolean b) {
-        System.setProperty("server.port", String.valueOf(config.getPort()));
-        APP_CTX = new SpringApplicationBuilder()
-                .bannerMode(Banner.Mode.OFF)
-                .sources(ServerApplication.class)
+        APP_CTX = new SpringApplicationBuilder(ServerApplication.class)
+//                .bannerMode(Banner.Mode.OFF)
                 .profiles("https")
                 .run(args);
     }
---
+++ server/src/main/java/org/crichton/configuration/CrichtonDataStorageProperties.java
@@ -0,0 +1,39 @@
+package org.crichton.configuration;
+
+import jakarta.annotation.PostConstruct;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.io.File;
+import java.util.Optional;
+
+@Getter
+@ConfigurationProperties(prefix = "crichton.data.storage")
+@RequiredArgsConstructor
+public class CrichtonDataStorageProperties {
+
+    private final Logger logger = LoggerFactory.getLogger(CrichtonDataStorageProperties.class);
+
+    private final Optional<String> basePath;
+
+    public String getBasePath() {
+        return basePath.orElse(System.getProperty("user.home") + File.separator + ".crichton" + File.separator + "data");
+    }
+
+    @PostConstruct
+    public void init() {
+        createDirectoryIfNotExists(getBasePath());
+    }
+
+
+    private void createDirectoryIfNotExists(String path) {
+        logger.info("Creating directory {}", path);
+        File directory = new File(path);
+        if (!directory.exists()) {
+            directory.mkdirs(); // 디렉토리가 없을 경우 생성
+        }
+    }
+}
