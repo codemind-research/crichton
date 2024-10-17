04ec7fd4cc94dde52028344e17877da4b3c3c9d3
juniair.kim <kkj@codemind.co.kr>
2024-10-17 17:35:48 +0900

    [feature] FileName에 plugin 관련 파일명 상수 정의

--- server/src/main/java/org/crichton/util/constants/FileName.java
+++ server/src/main/java/org/crichton/util/constants/FileName.java
@@ -1,11 +1,18 @@
 package org.crichton.util.constants;
 
 public class FileName {
+
+    //#region JSON
     public static final String TEST_SPEC = "test_spec.json";
     public static final String DEFECT_SPEC = "defect_spec.json";
     public static final String SAFE_SPEC = "safe_spec.json";
     public static final String UNIT_TEST_SPEC = "unit_test_spec.json";
+    //#endregion
 
+    //#region Plugin
+    public static final String INJECTOR_PLUGIN = "injector.jar";
+    public static final String UNIT_TESTER_PLUGIN = "coyote.jar";
+    //#endregion
 
     public static final String DEFECT_INJECTOR = "DefectInjector.dll";
 
@@ -15,6 +22,8 @@ public class FileName {
 
 
     public static final String INJECTION_RESULT = "injection_result.csv";
+
+
 
     private FileName() {
         throw new AssertionError("Cannot be instantiated");
