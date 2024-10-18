88ccd982cab41c30bc8c26124fc4defda0b11201
juniair.kim <kkj@codemind.co.kr>
2024-10-18 11:50:52 +0900

    [feature] JSON 파일 to Object 변환 관련 메서드 추가

--- server/src/main/java/org/crichton/util/ObjectMapperUtils.java
+++ server/src/main/java/org/crichton/util/ObjectMapperUtils.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
+import java.io.File;
+
 public class ObjectMapperUtils {
 
     private ObjectMapperUtils() {
@@ -14,6 +16,19 @@ public class ObjectMapperUtils {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
             return objectMapper.readValue(jsonString, valueType);
+        } catch (Exception e) {
+            throw new RuntimeException("Error converting JSON string to object", e);
+        }
+    }
+
+    public static <T> T convertJsonFileToObject(String jsonFile, Class<T> valueType) {
+        return convertJsonFileToObject(new File(jsonFile), valueType);
+    }
+
+    public static <T> T convertJsonFileToObject(File jsonFile, Class<T> valueType) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.readValue(jsonFile, valueType);
         } catch (Exception e) {
             throw new RuntimeException("Error converting JSON string to object", e);
         }
