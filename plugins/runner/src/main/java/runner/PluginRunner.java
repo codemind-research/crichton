6d7df0cefe04cd5ed74d2fec69682f51afd9811b
juniair.kim <kkj@codemind.co.kr>
2024-10-18 18:15:15 +0900

    [fix] 설정된 plugin 경로에 로그 남길 수 있도록 수정

--- plugins/runner/src/main/java/runner/PluginRunner.java
+++ plugins/runner/src/main/java/runner/PluginRunner.java
@@ -3,6 +3,7 @@ package runner;
 import lombok.NonNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import runner.dto.PluginOption;
 import runner.dto.ProcessedReportDTO;
 import runner.dto.RunResult;
 import runner.loader.BasicPluginLoader;
@@ -12,25 +13,30 @@ import runner.util.FileUtils;
 
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
-import java.util.MissingResourceException;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class PluginRunner implements Runner  {
 
     private static final Logger logger = LoggerFactory.getLogger(PluginRunner.class);
     private final String pluginName;
+    private final Path pluginLogFile;
     private final Path pluginJar;
     private final PluginLoader pluginLoader;
     private Plugin plugin;
 
     public PluginRunner(@NonNull String pluginName) throws Exception {
         this.pluginName = pluginName;
+        this.pluginLogFile = PluginPaths.generatePluginLogPath(PluginPaths.PLUGIN_DIR_PATH.resolve(pluginName));
         this.pluginJar = PluginPaths.generatePluginJarPath(pluginName);
         this.pluginLoader = new BasicPluginLoader(pluginJar);
     }
 
     public PluginRunner(@NonNull String pluginDirectory, @NonNull String pluginName) throws Exception {
-        this.pluginName = pluginName;
+        this.pluginName = pluginName.replace(".jar", "");
+        this.pluginLogFile = PluginPaths.generatePluginLogPath(Paths.get(pluginDirectory));
         this.pluginJar = PluginPaths.generatePluginJarPath(pluginDirectory, pluginName);
         this.pluginLoader = new BasicPluginLoader(pluginJar);
     }
@@ -65,16 +71,28 @@ public class PluginRunner implements Runner  {
     @Override
     public RunResult run(@NonNull String targetSource, Map<String, String> pluginSetting) {
         try {
-            plugin.initialize(pluginName, targetSource, pluginSetting);
+
+            UUID projectId = null;
+            if(pluginSetting.containsKey("project_id")) {
+                projectId = UUID.fromString(pluginSetting.get("project_id"));
+            }
+
+            var pluginOption = new PluginOption(projectId, pluginName, targetSource, pluginSetting, pluginLogFile);
+            plugin.initialize(pluginOption);
+
             String start = String.format("\nStart of Plugin : %s  \n", pluginName);
-            FileUtils.overWriteDump(PluginPaths.CRICHTON_LOG_PATH.toFile(),start,"\n");
+            FileUtils.overWriteDump(pluginLogFile.toFile(), start,"\n");
+
             boolean runResult = plugin.execute();
+
             ProcessedReportDTO data = plugin.transformReportData();
+
             String end = String.format("\nPlugin completed : %s  \n", pluginName);
-            FileUtils.overWriteDump(PluginPaths.CRICHTON_LOG_PATH.toFile(),end,"\n");
+            FileUtils.overWriteDump(pluginLogFile.toFile(),end,"\n");
+
             return new RunResult(runResult , data);
         }catch (Exception e) {
-            logger.error("Test Failed Plugin Run: " +pluginName);
+            logger.error("Test Failed Plugin Run: {}", pluginName);
             return new RunResult(false, new ProcessedReportDTO());
         }
     }
