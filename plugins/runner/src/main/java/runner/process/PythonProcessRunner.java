0e14b45309cf0c2a3ca28d71a4670e55bebfef83
juniair.kim <kkj@codemind.co.kr>
2024-10-19 18:49:19 +0900

    [feature] PythonProcessRunner 버전별 getProcessName 반환 값 처리하도록 구현
    - Python 2.x와 Python 3.x에 대해 반환값 분기하도록 구현
    - 둘다 설치 되어 있는 경우 Pytrhon3로 사용하도록 처리

--- plugins/runner/src/main/java/runner/process/PythonProcessRunner.java
+++ plugins/runner/src/main/java/runner/process/PythonProcessRunner.java
@@ -1,11 +1,54 @@
 package runner.process;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public abstract class PythonProcessRunner extends ProcessRunner {
 
-    private static final String PYTHON = "python";
+    private static final String PYTHON_VERSION_2 = "python";
+    private static final String PYTHON_VERSION_3 = "python3";
 
+    
     @Override
     protected String getProcessName() {
-        return PYTHON;
+        // TODO: 추후 properties를 통해 Python 경로로 해서 반환 처리 해야함    
+        boolean isPython2Installed = checkPython(PYTHON_VERSION_2);
+        boolean isPython3Installed = checkPython(PYTHON_VERSION_3);
+
+        if(isPython3Installed) {
+            return PYTHON_VERSION_3;
+        }
+        else {
+            return PYTHON_VERSION_2;
+        }
+    }
+
+
+    private static boolean checkPython(String command) {
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder(command, "--version");
+            Process process = processBuilder.start();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            System.out.println("Checking " + command + ":");
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            int exitCode = process.waitFor();
+            if (exitCode == 0) {
+                System.out.println(command + " is installed and working properly.\n");
+                return true;
+            } else {
+                System.out.println(command + " is not installed or there was an issue.\n");
+                return false;
+            }
+
+
+        } catch (Exception e) {
+            System.out.println(command + " is not available on this system.");
+            return false;
+        }
     }
 }
