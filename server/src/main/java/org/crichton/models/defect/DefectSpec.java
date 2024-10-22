1d52689d8f6dcee87fe66fc42b49474d83679093
juniair.kim <kkj@codemind.co.kr>
2024-10-22 15:10:23 +0900

    [fix] record 타입이므로 불피룡한 Lombok 어노테이션 제거

--- server/src/main/java/org/crichton/models/defect/DefectSpec.java
+++ server/src/main/java/org/crichton/models/defect/DefectSpec.java
@@ -4,7 +4,5 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 
-@Builder
-@Getter
 public record DefectSpec(int id, long trigger, long cycle, String target, @JsonProperty("defect") Spec spec) {
 }
--- server/src/main/java/org/crichton/models/defect/Spec.java
+++ server/src/main/java/org/crichton/models/defect/Spec.java
@@ -5,7 +5,5 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
-@Builder
-@Getter
 public record Spec(Bitflip bitflip) {
 }
