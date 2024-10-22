b435ef1037008265b24cf8e2ac0d37e6a53dfde4
juniair.kim <kkj@codemind.co.kr>
2024-10-22 17:59:45 +0900

    [feature] 메서드명 통일화

--- server/src/main/java/org/crichton/util/ObjectMapperUtils.java
+++ server/src/main/java/org/crichton/util/ObjectMapperUtils.java
@@ -9,6 +9,8 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,8 +27,16 @@ public class ObjectMapperUtils {
         throw new AssertionError();
     }
 
+    public static <T> List<T> convertJsonToList(File jsonFile, Class<T> valueType) throws IOException {
+        return convertJsonToList(jsonFile.toPath(), valueType);
+    }
+
+    public static <T> List<T> convertJsonToList(Path jsonFilePath, Class<T> valueType) throws IOException {
+        return convertJsonToList(Files.newInputStream(jsonFilePath), valueType);
+    }
+
     public static <T> List<T> convertJsonToList(InputStream inputStream, Class<T> clazz) {
-        mapper.configure(JsonParser.Feature.ALLOW_COMMENTS, true);
+
         List<T> result = new ArrayList<>();
 
         try {
@@ -46,9 +56,36 @@ public class ObjectMapperUtils {
             }
 
         }
-        catch (IOException e) {}
+        catch (IOException e) {
+            e.printStackTrace();
+        }
 
         return result;
+    }
+
+    public static <T> List<T> convertJsonContentToList(String jsonString, Class<T> valueType) {
+
+        List<T> result = new ArrayList<>();
+
+        try {
+            JsonNode jsonNode = mapper.readTree(jsonString);
+
+            // JSON이 배열인 경우
+            if (jsonNode.isArray()) {
+                // InputStream을 다시 읽기 위해 리셋하거나, 다시 받아야 함
+                // 새로운 InputStream을 사용하여 배열을 처리
+                result = mapper.readValue(jsonNode.toString(), mapper.getTypeFactory().constructCollectionType(List.class, valueType));
+            }
+            // JSON이 단일 객체인 경우
+            else {
+                T obj = mapper.treeToValue(jsonNode, valueType);
+                result.add(obj);  // 단일 객체를 리스트로 변환
+            }
+
+            return result;
+        } catch (Exception e) {
+            throw new RuntimeException("Error converting JSON string to object", e);
+        }
     }
 
     public static <T> T convertJsonStringToObject(String jsonString, Class<T> valueType) {
@@ -59,11 +96,15 @@ public class ObjectMapperUtils {
         }
     }
 
-    public static <T> T convertJsonFileToObject(String jsonFile, Class<T> valueType) {
-        return convertJsonFileToObject(new File(jsonFile), valueType);
+    public static <T> T convertJsonToObject(String jsonFile, Class<T> valueType) {
+        return convertJsonToObject(new File(jsonFile), valueType);
     }
 
-    public static <T> T convertJsonFileToObject(File jsonFile, Class<T> valueType) {
+    public static <T> T convertJsonToObject(Path jsonFile, Class<T> valueType) {
+        return convertJsonToObject(jsonFile.toFile(), valueType);
+    }
+
+    public static <T> T convertJsonToObject(File jsonFile, Class<T> valueType) {
         try {
             return mapper.readValue(jsonFile, valueType);
         } catch (Exception e) {
@@ -80,13 +121,7 @@ public class ObjectMapperUtils {
     }
 
     public static <T> void saveObjectToJsonFile(T object, String filePath) {
-        try {
-            mapper.writeValue(new File(filePath), object);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException("Error converting object to JSON string", e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        saveObjectToJsonFile(object, new File(filePath));
     }
 
     public static <T> void saveObjectToJsonFile(T object, File file) {
