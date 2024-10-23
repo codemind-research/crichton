1ed285731834d0271c6d82e29299c78ff0a22b38
juniair.kim <kkj@codemind.co.kr>
2024-10-23 15:54:18 +0900

    [fix] 정규표현식이 잘못되어 결과 파일을 반환 하지 못하는 버그 수정

--- plugins/coyote/src/main/java/coyote/util/RegexPatterns.java
+++ plugins/coyote/src/main/java/coyote/util/RegexPatterns.java
@@ -6,7 +6,7 @@ public class RegexPatterns {
 
     public static final Pattern PROJECT_PATTERN = Pattern.compile("Project Name,");
     public static final Pattern FILE_PATTERN = Pattern.compile("^Files,.*");
-    public static final Pattern UNIT_PATTERN = Pattern.compile("^FilePath,.*");
+    public static final Pattern UNIT_PATTERN = Pattern.compile("^File\\s*Path,.*");
     public static final Pattern TOTAL_PATTERN = Pattern.compile("^Total,.*");
 
 }
